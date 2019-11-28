9f945c79396291e24782943ad4032294e2fc4c8c
devina8 <jaiswal.de@husky.neu.edu>
2019-11-27 19:39:58 -0500

    third commit

--- Final_Project/src/main/java/Business/Role/PolicyManagerRole.java
+++ Final_Project/src/main/java/Business/Role/PolicyManagerRole.java
@@ -5,10 +5,22 @@
  */
 package Business.Role;
 
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.PolicyManagerOrganization;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import userinterface.FinanceManagerRole.FinanceManagerWorkAreaJpanel;
+import userinterface.PolicyManagerRole.PolicyManagerWorkAreaJPanel;
+
 /**
  *
  * @author devinajaiswal
  */
-public class PolicyManagerRole {
-    
+public class PolicyManagerRole extends Role {
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new PolicyManagerWorkAreaJPanel(userProcessContainer, account, (PolicyManagerOrganization)organization, enterprise);
+    }
 }
